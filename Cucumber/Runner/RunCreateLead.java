d058bbdfe06c058becc05e0b94b043c0c1a9baf4
swamycts <68296522+swamycts@users.noreply.github.com>
2020-08-16 13:25:28 +0530

    Add files via upload

---
+++ Cucumber/Runner/RunCreateLead.java
@@ -0,0 +1,9 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features="src/test/java/features/CreateLead.feature", glue="steps", monochrome=true)
+public class RunCreateLead extends AbstractTestNGCucumberTests {
+
+}
