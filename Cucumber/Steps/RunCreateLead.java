f56b5acc8214f662bce33f550560242e068c1f7f
swamycts <68296522+swamycts@users.noreply.github.com>
2020-08-16 13:23:37 +0530

    Delete RunCreateLead.java

--- Cucumber/Steps/RunCreateLead.java
+++
@@ -1,9 +0,0 @@
-package runner;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features="src/test/java/features/CreateLead.feature", glue="steps", monochrome=true)
-public class RunCreateLead extends AbstractTestNGCucumberTests {
-
-}
