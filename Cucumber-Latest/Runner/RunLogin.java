09fcb731c0536d4387e9a55bee8befde166a20a0
swamycts <68296522+swamycts@users.noreply.github.com>
2020-08-18 11:19:15 +0530

    Add files via upload

--- Cucumber-Latest/Runner/RunLogin.java
+++ Cucumber-Latest/Runner/RunLogin.java
@@ -3,7 +3,7 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features="src/test/java/features",
+@CucumberOptions(features= {"src/test/java/features/TC-002-CreateLead.feature","src/test/java/features/TC-003-EditLead.feature"},
                              glue="steps", 
                              monochrome=true,
                              plugin = {"html:report"}
